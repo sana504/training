8de2dea125e8d0269b1d4f740d818ad7454083a9
sana504 <168469540+sana504@users.noreply.github.com>
2025-07-28 10:12:42 +0500

    add

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Material(child: Center(child: Text("Iqra College"))),
     );
   }
 }
