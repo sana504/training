15ddb134efdc889e8415ab6fd137d22504fbd49e
sana504 <168469540+sana504@users.noreply.github.com>
2025-08-20 12:25:29 +0500

    container

--- lib/login.dart
+++ lib/login.dart
@@ -6,27 +6,25 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(backgroundColor: Colors.red),
       body: Center(
-        
-        
         child: Container(
-          
           height: 200,
           margin: EdgeInsets.symmetric(horizontal: 15),
           width: double.infinity,
-          
           decoration: BoxDecoration(
-
             color: Colors.blue,
-            
+
             borderRadius: BorderRadius.circular(10),
-            
-            image: DecorationImage(image: AssetImage("assets/images.jpg"),fit: BoxFit.cover)
+
+            image: DecorationImage(
+              image: AssetImage("assets/images.jpg"),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-        
-        
+
         // child: Image.asset("assets/images.jpg",scale: 0.5,),
         // child: Image.network("https://c02.purpledshub.com/uploads/sites/40/2023/08/JI230816Cosmos220-6d9254f-edited-scaled.jpg?w=1029&webp=1",scale: 0.5,),
         // child: Text(
